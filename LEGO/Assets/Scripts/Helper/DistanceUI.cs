f1dd553912eded801ce0714b74f343197c11c389
DitzelGames <xxxditzelxxx@gmail.com>
2019-08-08 21:46:46 +0200

    LegoGame 3

---
+++ LEGO/Assets/Scripts/Helper/DistanceUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class DistanceUI : MonoBehaviour
+{
+    public Vector3 Distance;
+    public Transform parent;
+    public Transform current;
+
+    private void Update()
+    {
+        if(current != null && parent != null)
+            Distance = current.position  - parent.position;
+    }
+}
