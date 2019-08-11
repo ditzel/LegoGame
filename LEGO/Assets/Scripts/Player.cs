41c7b5998a3fe1d384835f5a23a72a630e96da20
DitzelGames <xxxditzelxxx@gmail.com>
2019-08-11 15:45:29 +0200

    Lego Game 5

--- LEGO/Assets/Scripts/Player.cs
+++ LEGO/Assets/Scripts/Player.cs
@@ -20,8 +20,12 @@ public class Player : MonoBehaviour
     public const float Speed = 10f;
     public const float JumpForce = 7f;
 
+    public float FallMultiplier = 3f;
+    public float JumpMulitplier = 2f;
+
     protected Rigidbody Rigidbody;
     protected Quaternion LookRotation;
+    protected bool CanJump;
 
     private void Awake()
     {
@@ -42,13 +46,23 @@ public class Player : MonoBehaviour
 
         transform.rotation = LookRotation;
 
-        if (Input.Jump)
+        //Jump
+        Rigidbody.velocity += Vector3.up * Physics.gravity.y * (Rigidbody.velocity.y < 0 ? (FallMultiplier - 1) : (JumpMulitplier - 1) * (Input.Jump ? 0f : 1f))* Time.fixedDeltaTime;
+        CanJump = CanJump || DistanceFromGroud() <= 0;
+
+        if (Input.Jump && CanJump)
         {
-            var grounded = Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, 0.2f, 1);
-            if (grounded)
-            {
-                Rigidbody.velocity = new Vector3(Rigidbody.velocity.x, JumpForce, Rigidbody.velocity.z);
-            }
+            CanJump = false;
+            Rigidbody.velocity = new Vector3(Rigidbody.velocity.x, JumpForce, Rigidbody.velocity.z);
         }
+
+    }
+
+    public float DistanceFromGroud()
+    {
+        if (Physics.Raycast(transform.position + Vector3.up * 0.2f, Vector3.down, out var hitinfo, 10f, 1))
+            return hitinfo.distance - 0.21f;
+        else
+            return float.MaxValue;
     }
 }
--- LEGO/Assets/Scripts/PlayerAnimation.cs
+++ LEGO/Assets/Scripts/PlayerAnimation.cs
@@ -6,19 +6,30 @@ public class PlayerAnimation : MonoBehaviour
 {
 
     protected Animator Animator;
+    protected Player Player;
     protected Rigidbody Rigidbody;
+
+    protected Vector3 LeanTowards;
+    protected Vector3 LeanTowardsVel;
 
     // Start is called before the first frame update
     void Awake()
     {
         Animator = GetComponent<Animator>();
-        Rigidbody = GetComponent<Rigidbody>();
+        Rigidbody = transform.parent.GetComponent<Rigidbody>();
+        Player = transform.parent.GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var lean = new Vector3(Player.Input.RunX, 0, Player.Input.RunZ);
+        LeanTowards = Vector3.SmoothDamp(LeanTowards, lean, ref LeanTowardsVel, 0.1f);
+
+        transform.rotation = Quaternion.FromToRotation(transform.parent.up, Vector3.up * 3f + LeanTowards) * transform.parent.rotation;
+
         Animator.SetFloat("Speed", Mathf.Sign(Vector3.Dot(Rigidbody.velocity, Rigidbody.transform.forward)) * Vector3.Project(Rigidbody.velocity, Rigidbody.transform.forward).magnitude);
         Animator.SetFloat("Side", Mathf.Sign(Vector3.Dot(Rigidbody.velocity, Rigidbody.transform.right)) * Vector3.Project(Rigidbody.velocity, Rigidbody.transform.right).magnitude);
+        Animator.SetFloat("Jump", Mathf.Clamp(Player.DistanceFromGroud(), 0, .2f) * 5f);
     }
 }
